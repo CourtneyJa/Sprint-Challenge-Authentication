5897b1b73a45e8856737becfe0891acaaea30ff6
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2020-02-25 20:08:31 -0500

    9 out of 10 tests working, I think I got MVP

---
+++ jokes/jokes-router.spec.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const server = require("../api/server");
+
+describe("GET /api/jokes", () => {
+  it("should be json", async () => {
+    const res = await request(server).get("/api/jokes");
+    expect(res.type).toBe("application/json");
+  });
+});
